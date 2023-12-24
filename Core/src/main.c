c6cfd6c5683edae761b8c451cf16a2b0f06b7cd5
hazimgharib21 <hazimgharib21@gmail.com>
2023-12-24 00:10:01 +0000

    added vscode settings file, add initial setup code and gpio

--- Core/src/main.c
+++ Core/src/main.c
@@ -2,16 +2,18 @@
 
 void Error_Handler(void);
 void SystemClock_Config(void);
+static void MX_GPIO_Init();
 
 int main()
 {
     HAL_Init();
 
     SystemClock_Config();
+    MX_GPIO_Init();
 
-    int val = 0;
     while(1){
-        val += 1;
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        HAL_Delay(1000);
     }
 }
 
@@ -54,6 +56,31 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
 void Error_Handler(void)
--- build_debug.sh
+++ build_debug.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+rm -rf Debug/
+
+echo
+echo "***Running***"
+echo "cmake -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake  -S ./ -B Debug -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug"
+echo
+
 cmake -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake  -S ./ -B Debug -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
-make -C Debug VERBOSE=1
+
+echo
+echo "***Running***"
+echo "make -C Debug VERBOSE=1"
+echo
+
+cmake --build Debug
